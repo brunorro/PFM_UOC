d8171936d232beeae05d8d4e0d0da25f2d1c8b90
brunorro <br1.rdgz@gmail.com>
2010-05-21 19:15:58 +0200

    Modificada la interfaz de reconocimiento para que los mensajes aparezcan en la barra de estado en lugar de con un simple print.

---
+++ Interfaz_Captura/MainWindow.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+import sys, time
+
+from Exceptions import *
+from CaptureDevice import *
+
+try:
+	from opencv import cv,highgui
+except:
+	raise OpenCVLibraryNotFoundException()
+
+try:
+	import gtk,pygtk
+	pygtk.require("2.0")
+except:
+	raise GTKLibraryNotFoundException()
+
+
+class MainWindow():
+
+	def __init__ (self, parentClass):
+		self.parentClass = parentClass
+		self.captureDevice = self.parentClass.captureDevice
+
+		self.builder = gtk.Builder()
+		self.builder.add_from_file("./resources/MainWindow.glade")
+		self.mainWindow = self.builder.get_object("MainWindow")
+		self.mainImage = self.builder.get_object("MainDrawingarea")
+		self.mainStatusbar = self.builder.get_object("MainStatusbar")
+
+		self.showFeaturesCheckmenuitem = self.builder.get_object("ShowFeaturesCheckmenuitem")
+		self.showFacesCheckmenuitem = self.builder.get_object("ShowFacesCheckmenuitem")
+		self.grayCheckmenuitem = self.builder.get_object("GrayCheckmenuitem")
+
+		self.showFeaturesCheckmenuitem.set_active(self.parentClass.config.recoPreferences['ShowFeatures'])
+		self.showFacesCheckmenuitem.set_active(self.parentClass.config.recoPreferences['ShowFaces'])
+		self.grayCheckmenuitem.set_active(self.parentClass.config.recoPreferences['Gray'])
+
+		if self.mainWindow:
+			self.mainWindow.connect("destroy", self.parentClass.quit)
+
+		self.builder.connect_signals(self)
+		#self.redrawCounter = 0
+		#self.initTime = time.time()
+		#self.salto=0
+
+	def on_PreferencesImagemenuitem_activate(self,widget):
+		self.parentClass.openPreferencesDialog()
+
+#	def set_original_size(self, widget):
+#		self.MainImage.window.resize(640,480)
+
+	def on_MainDrawingarea_expose_event(self, widget, data):
+		
+		[img_width,img_height] = self.mainImage.window.get_size()
+		if self.captureDevice!=None:
+			faces = self.captureDevice.getFaces()
+
+			if len(faces)>0:
+				self.mainStatusbar.push(0,"Individuo detectado")
+			else:
+				self.mainStatusbar.push(0,"")
+			#if time.time()-self.initTime >= 30.0:
+			#	self.redrawCounter = 0
+			#	self.initTime=time.time()
+			#else:
+			#	self.redrawCounter+=1
+
+        		self.mainImage.window.draw_pixbuf(None,(self.parentClass.captureDevice.getPixbuf()).scale_simple(img_width, img_height,gtk.gdk.INTERP_NEAREST),0,0,0,0)
+			self.mainImage.queue_draw()
+		else:
+			self.mainStatusbar.push(0, "Camara no configurada")
+
+	def on_QuitImagemenuitem_activate(self,widget):
+		self.parentClass.quit()
+
+	def exceptionMessageDialog (self, title, msg):
+		md= gtk.MessageDialog(self.mainWindow, gtk.DIALOG_MODAL, gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE, msg)
+		md.set_title(title)
+		md.run()
+		md.destroy()
+
+	def on_ShowFeaturesCheckmenuitem_toggled(self, widget, data=None):
+		if self.showFeaturesCheckmenuitem.get_active():
+			self.mainStatusbar.push(0, "Encuadrar rasgos activado")
+			self.captureDevice.drawFeatures=1
+		else:
+			self.mainStatusbar.push(0, "Encuadrar rasgos desactivado")
+			self.captureDevice.drawFeatures=0
+
+
+	def on_ShowFacesCheckmenuitem_toggled(self, widget, data=None):
+		if self.showFacesCheckmenuitem.get_active():
+			self.showFeaturesCheckmenuitem.set_inconsistent(False)
+			self.showFeaturesCheckmenuitem.set_active(True)
+			self.captureDevice.drawFaces=1
+			self.mainStatusbar.push(0, "Encuadrar caras activado")
+
+		else:
+			self.showFeaturesCheckmenuitem.set_inconsistent(True)
+			self.captureDevice.drawFaces=0
+			self.mainStatusbar.push(0, "Encuadrar caras desactivado")
+
+	def on_GrayCheckmenuitem_toggled(self, widget, data=None):
+		if self.grayCheckmenuitem.get_active():
+			self.captureDevice.gray=1
+			self.mainStatusbar.push(0, "Imagen en escala de grises")
+		else:
+			self.captureDevice.gray=0
+			self.mainStatusbar.push(0, "Imagen en color")
---
+++ Interfaz_Captura/Prototipo_Captura.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+from Exceptions import *
+from CaptureDevice import *
+from MainWindow import *
+from PreferencesDialog import *
+from Configuration import *
+
+class Reconocedor:
+	
+	def __init__(self):
+
+		self.config = Configuration("./resources/prueba.xml")
+		self.prefDialog = PreferencesDialog(self.config)
+
+		self.captureDevice= CaptureDevice(self)
+		self.MW = MainWindow(self)
+
+		self.setCaptureDevice()
+
+		self.MW.mainWindow.show()
+
+		gtk.main()
+
+	def openPreferencesDialog(self, tab=0):
+		cameraAux = self.config.cameraDevice
+
+		self.prefDialog.preferencesDialog.run()
+		self.prefDialog.preferencesDialog.hide()
+
+		if self.config.cameraDevice!=cameraAux:
+			self.setCaptureDevice()
+
+	def setCaptureDevice(self):
+		try :
+			self.captureDevice.setCaptureParameters(self.config.cameraDevice, self.config.cascadeFile)
+
+		except CaptureDeviceParametersException ,e:
+			self.MW.exceptionMessageDialog("Errores en dispositivo de captura", e.__str__())
+			self.openPreferencesDialog(0)
+
+
+	def quit(self, widget=None):
+		gtk.main_quit()
+
+if __name__=="__main__":
+	r  = Reconocedor()
